920adf018e0cc972078a4cab37df115f2d11ebab
Hayamnoids <haymanoid.w@gmail.com>
2017-06-28 16:25:58 +1200

    Sync Session 6

--- will.game.py
+++ will.game.py
@@ -3,56 +3,65 @@
 
 #background is background1
 
-import pygame #load keywords from file
-import sys #python can now use file system
-import os #lets python understand your OS
-'''OBJECTS'''
-#CASSES AND FUNCTIONS
+import pygame
+import sys
+import os
+
+'''
+Objects
+'''
 
 class Player(pygame.sprite.Sprite):
-    #spawn user
     def __init__(self):
-        pygame.sprite.sprite.__init__(self)
-        self.images = [ ]
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
         img = pygame.image.load(os.path.join('images','hero.png')).convert()
+        img.convert_alpha()
+        img.set_colorkey(alpha)
         self.images.append(img)
         self.image = self.images[0]
-        self.rect = self.image.get_rect() 
+        self.rect  = self.image.get_rect()
 
-
-'''SETUP'''
-
-screenX = 960         #CODE RUNS AT ONCE
+'''
+Setup
+'''
+alpha = (0,0,0)
+black = (0,0,0)
+white = (255,255,255)
+screenX = 960
 screenY = 720
-fps = 40    
-afps = 4                    #<------{fps rate
-clock = pygame.time.Clock() #       {animation cycles
-pygame.init()
 
+fps = 40        # frame rate
+afps = 4        # animation cycles
+clock = pygame.time.Clock()
+pygame.init()
 main = True
 
-screen = pygame.display.set_mode([screenX, screenY])
-background = pygame.image.load(os.path.join('images','background1.png')).convert()
-backgroundRect = screen.get_rect()
-
-player = player()
+screen = pygame.display.set_mode([screenX,screenY])
+backdrop = pygame.image.load(os.path.join('images','background1.png')).convert()
+backdropRect = screen.get_rect()
+player = Player()   # spawn player
 player.rect.x = 0
 player.rect.y = 0
 movingsprites = pygame.sprite.Group()
-movingsprites.add(player) 
-'''MAIN LOOP'''
+movingsprites.add(player)
 
+'''
+Main loop
+'''
 while main == True:
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit(); sys.exit()
+            main = False
+
         if event.type == pygame.KEYUP:
             if event.key == ord('q'):
                 pygame.quit()
                 sys.exit()
-                main = False 
+                main = False
 
-    screen.blit(background, backgroundRect)
-    
-
+    screen.blit(backdrop, backdropRect)
+    movingsprites.draw(screen)
     pygame.display.flip()
     clock.tick(fps)
-
